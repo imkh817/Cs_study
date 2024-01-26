428729516f7750bcc4f5ba0f6f3fc55c143cff67
imkh817 <rjs7180@naver.com>
2024-01-27 08:28:35 +0900

    CS 공부

---
+++ src/main/java/home/cs/StudyCsApplication.java
@@ -0,0 +1,13 @@
+package home.cs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyCsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyCsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/home/cs/StudyCsApplicationTests.java
@@ -0,0 +1,13 @@
+package home.cs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyCsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
